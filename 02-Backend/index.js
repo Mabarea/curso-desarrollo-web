123a9d1b133528724206a106d1d890246d6ea2f9
mabarea <miguel.angel.barea.ortega@gmail.com>
2026-02-05 12:47:05 +0100

    Día 2: Explicación detallada de fs.readFile y asincronía

--- 02-Backend/index.js
+++ 02-Backend/index.js
@@ -6,3 +6,21 @@ const fecha = herramientas.obtenerFecha();
 
 console.log(mensaje);
 console.log(`Hoy es día ${fecha}`);
+
+
+//LECTURA DE ARCHIVOS
+
+const fs = require('fs'); // Importamos el módulo nativo de archivos
+
+console.log("1. El servidor inicia la lectura...");
+
+//Leamos el archivo de forma ASÍNCRONA
+fs.readFile('datos.txt', 'utf-8', (error,contenido) => {
+    if (error) {
+        console.error("Hubo un fallo al leer:", error);
+        return;
+    }
+    console.log("2. Contenido del archivo:", contenido);
+});
+
+console.log("3. El servidor sigue trabajando mientras se lee el archivo.");
